954f324ebca4489c0d71f2a47f85ec222f62c0d2
mdshahed89 <mdshahedprofile@gmail.com>
2024-08-08 11:04:28 +0600

    cookie updated

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -45,7 +45,7 @@ export const signin = async (req, res, next) => {
     const { password: hashedPassword, ...rest } = validUser._doc;
     const expiryDate = new Date(Date.now() + 2592000000000); // 1 hour
     res
-      .cookie('access_token', token, { httpOnly: true, expires: expiryDate })
+      .cookie('access_token', token, { httpOnly: true, expires: expiryDate, secure: false, sameSite: 'strict' })
       .status(200)
       .json(rest);
   } catch (error) {
--- api/index.js
+++ api/index.js
@@ -16,13 +16,13 @@ dotenv.config();
 
 const app = express();
 
+app.use(cookieParser());
 app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 
 // const port = process.env.PORT || 3000;
 
